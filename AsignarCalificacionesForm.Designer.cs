4cd33d2c13fcbc09623f9aae469b06b467c4a458
RamsiMiam <80412858+RamsiMiam@users.noreply.github.com>
2021-06-29 22:47:41 -0500

    Cmbios Asignar Calificación

--- AsignarCalificacionesForm.Designer.cs
+++ AsignarCalificacionesForm.Designer.cs
@@ -51,7 +51,7 @@ namespace Faculty
             this.cmbAlumnos.Location = new System.Drawing.Point(26, 126);
             this.cmbAlumnos.Margin = new System.Windows.Forms.Padding(4);
             this.cmbAlumnos.Name = "cmbAlumnos";
-            this.cmbAlumnos.Size = new System.Drawing.Size(322, 28);
+            this.cmbAlumnos.Size = new System.Drawing.Size(322, 25);
             this.cmbAlumnos.TabIndex = 0;
             this.cmbAlumnos.SelectedIndexChanged += new System.EventHandler(this.cbAlumnos_SelectedIndexChanged);
             // 
@@ -64,7 +64,7 @@ namespace Faculty
             this.label4.ForeColor = System.Drawing.SystemColors.Desktop;
             this.label4.Location = new System.Drawing.Point(282, 32);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(310, 38);
+            this.label4.Size = new System.Drawing.Size(265, 34);
             this.label4.TabIndex = 6;
             this.label4.Text = "Asignar calificación";
             // 
@@ -73,7 +73,7 @@ namespace Faculty
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(24, 40);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(68, 20);
+            this.label1.Size = new System.Drawing.Size(58, 17);
             this.label1.TabIndex = 8;
             this.label1.Text = "Anterior";
             // 
@@ -97,7 +97,7 @@ namespace Faculty
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(24, 115);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(56, 20);
+            this.label2.Size = new System.Drawing.Size(49, 17);
             this.label2.TabIndex = 8;
             this.label2.Text = "Nueva";
             // 
@@ -105,7 +105,7 @@ namespace Faculty
             // 
             this.nupNuevaC.Location = new System.Drawing.Point(24, 150);
             this.nupNuevaC.Name = "nupNuevaC";
-            this.nupNuevaC.Size = new System.Drawing.Size(194, 27);
+            this.nupNuevaC.Size = new System.Drawing.Size(194, 23);
             this.nupNuevaC.TabIndex = 13;
             this.nupNuevaC.ValueChanged += new System.EventHandler(this.nupNuevaC_ValueChanged);
             // 
@@ -114,7 +114,7 @@ namespace Faculty
             this.tbAnteriorC.Enabled = false;
             this.tbAnteriorC.Location = new System.Drawing.Point(24, 64);
             this.tbAnteriorC.Name = "tbAnteriorC";
-            this.tbAnteriorC.Size = new System.Drawing.Size(194, 27);
+            this.tbAnteriorC.Size = new System.Drawing.Size(194, 23);
             this.tbAnteriorC.TabIndex = 9;
             // 
             // btnCerrar
@@ -146,24 +146,24 @@ namespace Faculty
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(22, 101);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(65, 20);
+            this.label3.Size = new System.Drawing.Size(55, 17);
             this.label3.TabIndex = 11;
             this.label3.Text = "Alumno";
             // 
             // lstMaterias
             // 
             this.lstMaterias.FormattingEnabled = true;
-            this.lstMaterias.ItemHeight = 20;
+            this.lstMaterias.ItemHeight = 17;
             this.lstMaterias.Location = new System.Drawing.Point(26, 178);
             this.lstMaterias.Name = "lstMaterias";
-            this.lstMaterias.Size = new System.Drawing.Size(500, 304);
+            this.lstMaterias.Size = new System.Drawing.Size(500, 293);
             this.lstMaterias.TabIndex = 12;
             this.lstMaterias.SelectedIndexChanged += new System.EventHandler(this.lstMaterias_SelectedIndexChanged);
             // 
             // AsignarCalificacionesForm
             // 
             this.AcceptButton = this.btnActualizar;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.btnCerrar;
             this.ClientSize = new System.Drawing.Size(791, 507);
@@ -175,9 +175,12 @@ namespace Faculty
             this.Controls.Add(this.label4);
             this.Controls.Add(this.cmbAlumnos);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
             this.Name = "AsignarCalificacionesForm";
-            this.Text = "AsignarCalificacionesForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Asignar calificación";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.nupNuevaC)).EndInit();
